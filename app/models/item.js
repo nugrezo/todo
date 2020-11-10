568a411ab6bfe859be9a87dfa217773e08e56468
Ergun Oz <ergun.oz82@gmail.com>
2020-11-09 21:44:30 -0500

    working on a todo project

---
+++ app/models/item.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const itemSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+const Item = mongoose.model('Item', itemSchema)
+
+module.exports = Item
